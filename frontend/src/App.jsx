3675c96cb476ffb3fbb3aff670114fe78676f0fd
kentatixx <kentatixx@gmail.com>
2025-06-16 14:47:39 +0800

    installing vite and tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-black h-screen">
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-purple-700 text-[200px] text-center ">WELCOMEE <br/>  
+        <span className="animate-pulse"> JOVANIEEEE</span></h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
