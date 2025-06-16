72977a50e7650f3b8589dd37b499b8e8ec7b4dac
kentatixx <kentatixx@gmail.com>
2025-06-16 14:57:18 +0800

    isntalling vite and tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,10 @@ const App = () => {
   return (
     <div className="bg-black h-screen">
       <div className="flex items-center justify-center h-screen">
-        <h1 className="text-purple-700 text-[200px] text-center ">WELCOMEE <br/>  
-        <span className="animate-pulse"> JOVANIEEEE</span></h1>
+        <h1 className="text-purple-700 text-[200px] text-center ">
+          WELCOMEE <br />
+          <span className="animate-pulse"> JOVANIEE</span>
+        </h1>
       </div>
     </div>
   );
