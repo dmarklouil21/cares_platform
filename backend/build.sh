83988b8c896a6b35b38fddad0d0cc865178239b5
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-11-19 22:50:48 +0800

    minor fixes

--- backend/build.sh
+++ backend/build.sh
@@ -3,11 +3,13 @@
 set -o errexit
 
 # Create and activate virtual environment
-python -m venv venv
-source venv/bin/activate
+# python -m venv venv
+# source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 
 # Install dependencies
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
