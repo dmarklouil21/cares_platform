aaeba8eba1a7ba080a656959751dc63fac428709
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-11-20 21:30:15 +0800

    change email port

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -235,7 +235,8 @@ TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
 # Email backend settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+# EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = 'caresplatform@gmail.com'  #  Gmail address
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
