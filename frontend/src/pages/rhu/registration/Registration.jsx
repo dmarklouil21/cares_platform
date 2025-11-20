505d72a8047c601e2266d12a0d482f31f9c65178
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-11-20 23:59:24 +0800

    asdasd

--- frontend/src/pages/rhu/registration/Registration.jsx
+++ frontend/src/pages/rhu/registration/Registration.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import dateIcon from "../assets/images/input_icons/datebirth.svg";
 import emailIcon from "../assets/images/input_icons/email.svg";
 import phoneIcon from "../assets/images/input_icons/mobile.svg";
 
