a9a4f19d32bfe3e7a228516d4b58f6fa444de08e
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-11-20 17:37:53 +0800

    fix import on svgr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from 'vite-plugin-svgr'
 import path from "path";
 
 // https://vite.dev/config/
