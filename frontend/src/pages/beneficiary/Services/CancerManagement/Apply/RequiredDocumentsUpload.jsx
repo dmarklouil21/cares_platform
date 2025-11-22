1fd5efdaaad6b716a257ced9a9d4c85680b80acc
Mark Louil Diacamos <dmarklouil@gmail.com>
2025-11-22 22:41:21 +0800

    revert

--- frontend/src/pages/beneficiary/Services/CancerManagement/Apply/RequiredDocumentsUpload.jsx
+++ frontend/src/pages/beneficiary/Services/CancerManagement/Apply/RequiredDocumentsUpload.jsx
@@ -166,8 +166,8 @@ const RadioactiveDocument = () => {
         
         const method = isResubmitting ? api.patch : api.post;
 
-      // await method(endpoint, formData, { headers: { "Content-Type": "multipart/form-data" } });
-      await method(endpoint, formData);
+      await method(endpoint, formData, { headers: { "Content-Type": "multipart/form-data" } });
+      // await method(endpoint, formData);
 
         navigate("/beneficiary/applications/cancer-treatment", {
         state: {
